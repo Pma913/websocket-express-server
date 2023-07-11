c9988b7f0996696ff6d6aab46bfdb6ed15d486d1
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-07-11 10:59:20 -0400

    Update cors method

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const http = require("http")
 const { Server } = require('socket.io')
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
 const server = http.createServer(app)
 
 const io = new Server(server, {
